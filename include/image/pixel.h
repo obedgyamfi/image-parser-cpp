4e233d3fdbe10b40a5ae145d491cf5913a3c91f0
obedgyamfi <obedgyamfi53@gmail.com>
2025-06-29 12:40:10 +0000

    Created .gitignore and added .vs/

---
+++ include/image/pixel.h
@@ -0,0 +1,12 @@
+#ifndef IMAGE_PIXEL_H
+#define IMAGE_PIXEL_H 
+
+#include<cstdint>
+
+struct Pixel {
+    uint16_t r;
+    uint8_t g; 
+    uint8_t b;
+};
+
+#endif // IMAGE_PIXEL_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Image parser" << std::endl;
+
+    return 0;
+}
