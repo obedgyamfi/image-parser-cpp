ce1124bfac3c1924c174ae766538e941ab5c6ca8
obedgyamfi <obedgyamfi53@gmail.com>
2025-07-02 20:16:44 +0000

    Clean up: Scrapped the entire code, starting from scratch

--- main.cpp
+++ main.cpp
@@ -1,35 +1,9 @@
 #include <iostream>
-#include <string>
 
-#include "include/utils/Logger.hpp"
-#include "include/utils/CLI.hpp"
-// #include "include/utils/ImageUtils.hpp"
-#include "include/Image.hpp"
-#include "include/pngParser.hpp"
+int main()
+{
 
-
-int main(int argc , char* argv[]) {
-    // initiailizing classes 
-    CLI cli = CLI();
-    PngParser png_parser = PngParser();
-    CLIOptions opts = cli.parse(argc, argv);
-    
-    std::string image_path = opts.inputPath;
-
-    std::vector<unsigned char> imgBuff = png_parser.read(image_path);
-    // Image img = png_parser.parse(imgBuff);
-
-    for(auto c : imgBuff) {
-        std::cout << c << " ";
-    }
-
-    // Image img = Image(opts.inputPath);
-
-
-
-
-    
-
+    std::cout << "Image parser" << std::endl;
 
     return 0;
 }
